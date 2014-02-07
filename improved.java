dca4723b3465c9f56ceb288db0ccb8681005f47e
ShintaroTamura <tamura_shintaro@cygames.co.jp>
2014-02-07 18:41:37 +0900

    Extract statement method
    Calculation of thisAmount in statement method is moved into a new method
    "amountFor". In addition, .gitignore is updated not to stage .swp files.

--- improved.java
+++ improved.java
@@ -67,21 +67,8 @@ class Customer {
          double thisAmount = 0;
 	 Rental each = (Rental) rentals.nextElement();
 
-	 switch (each.getMovie().getPriceCode()) {
-	    case Movie.REGURAR:
-	       thisAmount += 2;
-	       if (each.getDaysRented() > 2)
-	          thisAmount += (each.getDaysRented() - 2) * 1.5;
-	       break;
-	    case Movie.CHILDRENS:
-	       thisAmount += 1.5;
-	       if (each.getDaysRented() > 3)
-	          thisAmount += (each.getDaysRented() - 3) * 1.5;
-	       break;
-	    case Movie.NEW_RELEASE:
-	       thisAmount += each.getDaysRented() * 3;
-	       break;
-	 }
+	 //calculate amount by using "amountFor" method.
+	 thisAmount = amountFor(each);
 
 	 frequentRenterPoints ++;
 
@@ -96,5 +83,25 @@ class Customer {
       result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " points.";
       return result;
+   }
+
+   public double amountFor(Rental each) {
+      double thisAmount;
+      switch (each.getMovie().getPriceCode()) {
+         case Movie.REGURAR:
+	    thisAmount += 2;
+	    if (each.getDaysRented() > 2)
+	       thisAmount += (each.getDaysRented() - 2) * 1.5;
+	    break;
+	 case Movie.CHILDRENS:
+	    thisAmount += 1.5;
+	    if (each.getDaysRented() > 3)
+	       thisAmount += (each.getDaysRented() - 3) * 1.5;
+	    break;
+	 case Movie.NEW_RELEASE:
+	    thisAmount += each.getDaysRented() * 3;
+	    break;
+      }
+      return thisAmount;
    }
 }
