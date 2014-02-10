9b49f6baca5f718aca94214756aefd953cde27e2
ShintaroTamura <tamura_shintaro@cygames.co.jp>
2014-02-10 17:11:34 +0900

    Replace a temp variable with an inquiry
    A temp variable "frequentRenterPoints" is replaced with an inquiry. A
    new method "getTotalFrequentRenterPoints" is added.

--- improved.java
+++ improved.java
@@ -87,19 +87,16 @@ class Customer {
    }
 
    public String statement() {
-      int frequentRenterPoints = 0;
       Enumeration rentals = _rentals.elements();
       String result = "Rental records for " + getName() + "\n";
       while(rentals.hasMoreElements()) {
 	 Rental each = (Rental) rentals.nextElement();
 
-	 frequentRenterPoints += each.getFrequentRenterPoints();
-
 	 result += "\t" + each.getMovie() + "\t" +
 	           String.valueOf(each.getCharge()) + "\n";
       }
       result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints) +
+      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " points.";
       return result;
    }
@@ -114,6 +111,16 @@ class Customer {
       while(rentals.hasMoreElements()) {
 	 Rental each = (Rental) rentals.nextElement();
 	 result += each.getCharge();
+      }
+      return result;
+   }
+
+   private int getTotalFrequentRenterPoints() {
+      int result = 0;
+      Enumeration rentals = _rentals.elements();
+      while(rentals.hasMoreElements()) {
+	 Rental each = (Rental) rentals.nextElement();
+	 result += each.getFrequentRenterPoints();
       }
       return result;
    }
