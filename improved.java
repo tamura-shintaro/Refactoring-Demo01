48ed050f3df07db46cc8ef0a7448c4dbb4fd8c29
ShintaroTamura <tamura_shintaro@cygames.co.jp>
2014-02-12 11:28:24 +0900

    Applying Polymorphism
    Move switch sentences in "getCharge" method and
    "getFrequnetRenterPoints" method.

--- improved.java
+++ improved.java
@@ -39,50 +39,54 @@ public class Movie {
       return _price.getCharge(daysRented);
    }
 
-   int getFrequentRenterPoints() {
-      if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-         return 2;
-      else
-         return 1;
+   int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
    }
 }
 
-abstract class Price {
+class Price {
    abstract int getPriceCode();
 
-   double getCharge(int daysRented) {
-      double result = 0;
-      switch (getPriceCode()) {
-         case Movie.REGURAR:
-	    result += 2;
-	    if (daysRented > 2)
-	       result += (daysRented - 2) * 1.5;
-	    break;
-	 case Movie.CHILDRENS:
-	    result += 1.5;
-	    if (daysRented > 3)
-	       result += (daysRented - 3) * 1.5;
-	    break;
-	 case Movie.NEW_RELEASE:
-	    result += daysRented * 3;
-	    break;
-      }
-      return result;
+   abstract double getCharge(int daysRented);
+
+   int getFrequentRenterPoints(int daysRented) {
+      return 1;
    }
 }
+
 class ChildrensPrice extends Price {
    int getPriceCode() {
       return Movie.CHILDRENS;
    }
+   double getCharge(int daysRented) {
+      double result = 1.5;
+      if (daysRented > 3)
+         result += (daysRented - 3) * 1.5;
+      return result;
+   }
 }
+
 class NewReleasePrice extends Price {
    int getPriceCode() {
       return Movie.NEW_RELEASE;
    }
+   double getCharge(int daysRented) {
+      return daysRented * 3;
+   }
+   int getFrequentRenterPoints(int daysRented) {
+      return (daysRented > 1) ? 2 : 1;
+   }
 }
+
 class RegularPrice extends Price {
    int getPriceCode() {
       return Movie.REGURAR;
+   }
+   double getCharge(int daysRented) {
+      double result = 2;
+      if (daysRented > 2)
+         result += (daysRented - 2) * 1.5;
+      return result;
    }
 }
 
